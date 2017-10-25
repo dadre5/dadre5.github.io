69752f788e65bf97ceadd7bc9f2d7d4c09468209
dadre5 <r.severino1993@gmail.com>
2017-10-25 11:53:00 -0300

    Update share.php

--- share.php
+++ share.php
@@ -1,16 +1,19 @@
+<?php
+     $score = $_GET['score'];   //read the score from the url
+?>
 <!DOCTYPE html>
 <head>
-     <!-- This url should be the same as the href you passed in to showDialog -->
-     <meta property="og:url" content="<?= "http://". $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" /> 
-
-     <!-- Here I customized the title, but you can customize any property you want -->
-     <meta property="og:title" content="I scored <?= $score ?> points on Level <?= $level ?>!"/>
+    
+<!-- Here I customized the title, but you can customize any property you want -->
+     <meta property="og:title" content="I scored <?= $score ?> points!"/>
      <meta property="og:description" content="This is literally the best game on Earth! Download it now!" /> 
-     <meta property="og:image" content="http://yoursite.com/appimage.png" />
-	
+     <meta property="og:image" content="https://dadre5.github.io/Ballooooon.png" />
+<!-- This url should be the same as the href you passed in to showDialog -->
+     <meta property="og:url" content="https://www.attmotion.com" /> 
+
      <!-- Manually redirect to the page you want the user to land on. This is optional -->
-     <meta http-equiv="refresh" content="0;url=http://yoursite.com">
+     <meta http-equiv="refresh" content="0;url=http://www.attmotion.com">
      <script type="text/javascript">
-	    window.location.href = "http://yoursite.com"
+	    window.location.href = "http://www.attmotion.com"
      </script>
 </head>
