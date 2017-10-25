71a29433acccbd051b03c27c98e597f57aed36af
dadre5 <r.severino1993@gmail.com>
2017-10-25 17:59:22 -0300

    Update share.php

--- share.php
+++ share.php
@@ -9,7 +9,7 @@
      <meta property="og:url" content="" /> 
 	
 <!-- Here I customized the title, but you can customize any property you want -->
-     <meta property="og:title" content= <?= $points ?> />
+     <meta property="og:title" content= "titulo nuevo" />
      <meta property="og:image" content= <?= $picture ?> />
 	
      <meta property="og:description" content="This is literally the best game on Earth! Download it now!" /> 
